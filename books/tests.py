4997553d7d622fbc7f71437e7901321a7149e8a3
Axizeaus <axizeaus@gmail.com>
2022-09-08 22:02:13 +0630

    ADD tests for book content and template

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.urls import reverse 
 
-# Create your tests here.
+from .models import Book
+
+class BookTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.book = Book.objects.create(
+            title = 'test title',
+            subtitle = 'test subtitle', 
+            author = 'test author',
+            isbn = '1234567890123'
+        )
+        
+    def test_book_content(self):
+        self.assertEqual(self.book.title,'test title')
+        self.assertEqual(self.book.subtitle,'test subtitle')
+        self.assertEqual(self.book.author,'test author')
+        self.assertEqual(self.book.isbn,'1234567890123')
+        
+    def test_book_listview(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'test title')
+        self.assertTemplateUsed(response, 'books/book_list.html')
